7c30c10705446b403eadb74aa0eb20be718f9eb9
DURJAY SAMRAT NANDAMUDI <111143303+durjaysamrat@users.noreply.github.com>
2025-02-03 22:37:44 +0530

    Update exception_task5.java

--- task_day_4/exception_task5.java
+++ task_day_4/exception_task5.java
@@ -1,10 +1,31 @@
 package task_day_4;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 public class exception_task5 {
+
+	private static Exception IOException = null;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+     File inFile=new File("src//task_day_4//input.dat");
+     FileInputStream fis=null;
+     
+     try {
+    	 fis=new FileInputStream(inFile);
+    	 int ch=fis.read();
+    	 if(ch==-1)
+    			 {
+    		 throw new IOException("End of file or empty file");  
+    			 }
+    	 else {
+    		 System.out.println("read successfull");
+    	 }
+     }
+     catch(IOException e)
+     {
+    	 System.out.println("IOException occurred");
+     }
 	}
 
 }
